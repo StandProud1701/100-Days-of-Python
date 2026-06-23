e397a021d3619490558d4dcf1ec22ce41c101359
StandProud1701 <luthf.7860@gmail.com>
2026-06-23 20:12:56 +0400

    Create Tip Calculator.py

---
+++ Day 2-Understanding Data Types/Tip Calculator.py
@@ -0,0 +1,6 @@
+print('Welcome to the tip calculator!')
+bill = float(input('What was the total bill?\n$'))
+tip = int(input('How much tip would you like to give? 10, 12, or 15?\n'))
+people = int(input('How many people to split the bill?\n'))
+eachpay = (bill +(bill*tip/100))/people
+print(f'Each person should pay: ${round(eachpay,2)}')
